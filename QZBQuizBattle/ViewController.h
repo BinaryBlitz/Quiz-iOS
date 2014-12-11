3b893bf4b233b4e8a4fe83788035aac5deaa5f78
andreymikhaylov <drumih@gmail.com>
2014-12-11 16:04:27 +0300

    Initial Commit

---
+++ QZBQuizBattle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QZBQuizBattle
+//
+//  Created by Andrey Mikhaylov on 11/12/14.
+//  Copyright (c) 2014 Andrey Mikhaylov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QZBQuizBattle/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QZBQuizBattle
+//
+//  Created by Andrey Mikhaylov on 11/12/14.
+//  Copyright (c) 2014 Andrey Mikhaylov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
