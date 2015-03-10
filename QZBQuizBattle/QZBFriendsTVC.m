14c3199c4285c52fc01028baacf18a0bc9789c3c
andreymikhaylov <drumih@gmail.com>
2015-03-10 13:32:58 +0300

    player friends requests
    Former-commit-id: 308df764c44794d5a618701ddd79a52772474358

--- QZBQuizBattle/QZBFriendsTVC.m
+++ QZBQuizBattle/QZBFriendsTVC.m
@@ -11,71 +11,98 @@
 #import "QZBServerManager.h"
 #import "QZBAnotherUser.h"
 #import "QZBPlayerPersonalPageVC.h"
+#import "QZBFriendsRequestsTVC.h"
 
 @interface QZBFriendsTVC ()
 
-@property (strong, nonatomic) NSArray *friends;  // QZBAnotherUser
-@property (strong, nonatomic) id<QZBUserProtocol> user;
+@property(strong, nonatomic) NSArray *friends;         // QZBAnotherUser
+@property(strong, nonatomic) NSArray *friendsRequests; // QZBAnotherUser
+@property(strong, nonatomic) id<QZBUserProtocol> user;
 
 @end
 
 @implementation QZBFriendsTVC
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    self.title = @"Друзья";
+  [super viewDidLoad];
+  self.title = @"Друзья";
 }
 
 - (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
-- (void)setFriendsOwner:(id<QZBUserProtocol>)user andFriends:(NSArray *)friends {
-    self.user = user;
-    self.friends = friends;
-    
-    [self.tableView reloadData];
-    
-    [[QZBServerManager sharedManager] GETAllFriendsOfUserWithID:self.user.userID
-        OnSuccess:^(NSArray *friends) {
-            self.friends = friends;
-            [self.tableView reloadData];
-        }
-        onFailure:^(NSError *error, NSInteger statusCode){
+- (void)setFriendsOwner:(id<QZBUserProtocol>)user
+                friends:(NSArray *)friends
+        friendsRequests:(NSArray *)friendsRequest {
 
-        }];
+  self.friendsRequests = friendsRequest;
+
+  [self setFriendsOwner:user andFriends:friends];
+}
+
+- (void)setFriendsOwner:(id<QZBUserProtocol>)user
+             andFriends:(NSArray *)friends {
+  self.user = user;
+  self.friends = friends;
+
+  [self.tableView reloadData];
+
+  [[QZBServerManager sharedManager] GETAllFriendsOfUserWithID:self.user.userID
+      OnSuccess:^(NSArray *friends) {
+        self.friends = friends;
+        [self.tableView reloadData];
+      }
+      onFailure:^(NSError *error, NSInteger statusCode){
+
+      }];
 }
 
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    //#warning Potentially incomplete method implementation.
-    // Return the number of sections.
+  //#warning Potentially incomplete method implementation.
+  // Return the number of sections.
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    //#warning Incomplete method implementation.
-    // Return the number of rows in the section.
+- (NSInteger)tableView:(UITableView *)tableView
+    numberOfRowsInSection:(NSInteger)section {
+  //#warning Incomplete method implementation.
+  // Return the number of rows in the section.
+
     return self.friends.count;
-}
+    
+  }
+  //
+  //    if(self.friendsRequests && self.friendsRequests.count>0){
+  //        return self.friendsRequests.count +self.friends.count+1;
+  //    }else{
+  //
+  //    return self.friends.count;
+  //    }
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView
          cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
     QZBFriendCell *cell =
-        [tableView dequeueReusableCellWithIdentifier:@"friendCell" forIndexPath:indexPath];
-
+    [tableView dequeueReusableCellWithIdentifier:@"friendCell"
+                                    forIndexPath:indexPath];
+    
     QZBAnotherUser *user = self.friends[indexPath.row];
-
+    
     [cell setCellWithUser:user];
-
     return cell;
+
+    
 }
 
 /*
 // Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath
+*)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
@@ -91,7 +118,8 @@ forRowAtIndexPath:(NSIndexPath *)indexPath {
         [tableView deleteRowsAtIndexPaths:@[indexPath]
 withRowAnimation:UITableViewRowAnimationFade];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new
+        // Create a new instance of the appropriate class, insert it into the
+array, and add a new
 row to the table view
     }
 }
@@ -99,7 +127,8 @@ row to the table view
 
 /*
 // Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath
+*)fromIndexPath
 toIndexPath:(NSIndexPath
 *)toIndexPath {
 }
@@ -107,7 +136,8 @@ toIndexPath:(NSIndexPath
 
 /*
 // Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath
+*)indexPath {
     // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
@@ -115,33 +145,49 @@ toIndexPath:(NSIndexPath
 
 #pragma mark - UITableViewDelegate
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+- (void)tableView:(UITableView *)tableView
+    didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+  [tableView deselectRowAtIndexPath:indexPath animated:YES];
 
-    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+  UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
 
-    if (![cell isKindOfClass:[QZBFriendCell class]]) {
-        return;
-    }
+  if (![cell isKindOfClass:[QZBFriendCell class]]) {
+    return;
+  }
 
-    QZBFriendCell *friendCell = (QZBFriendCell *)cell;
-    self.user = friendCell.user;
+  QZBFriendCell *friendCell = (QZBFriendCell *)cell;
+  self.user = friendCell.user;
 
-    [self performSegueWithIdentifier:@"showUserpage" sender:nil];
+  [self performSegueWithIdentifier:@"showUserpage" sender:nil];
 }
 
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before
+// In a storyboard-based application, you will often want to do a little
+// preparation before
 // navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    if ([segue.identifier isEqualToString:@"showUserpage"]) {
-        QZBPlayerPersonalPageVC *vc = segue.destinationViewController;
+  // Get the new view controller using [segue destinationViewController].
+  // Pass the selected object to the new view controller.
+  if ([segue.identifier isEqualToString:@"showUserpage"]) {
+    QZBPlayerPersonalPageVC *vc = segue.destinationViewController;
 
-        [vc initPlayerPageWithUser:self.user];
-    }
+    [vc initPlayerPageWithUser:self.user];
+  } else if([segue.identifier isEqualToString:@"showFriendsRequests"]){
+      
+      QZBFriendsRequestsTVC *destinationVC = (
+                                              QZBFriendsRequestsTVC *)segue.destinationViewController;
+      
+      [destinationVC setFriendsOwner:self.user andFriends:self.friendsRequests];
+      
+  }
+}
+
+#pragma mark - actions
+- (IBAction)showFriendsRequestsAction:(id)sender {
+    
+    [self performSegueWithIdentifier:@"showFriendsRequests" sender:nil];
+    
 }
 
 @end
