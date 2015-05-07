f03dce4a8b8510d4ee0031f651cd028a292b6953
andreymikhaylov <drumih@gmail.com>
2015-05-07 12:35:38 +0300

    username check bug fix
    Former-commit-id: 0ae4ecc7561c886ed4837d3c9ee484c74f8f7f4c
    Former-commit-id: f02191f6d14d5e838aa357f9b3d80dd8bc00c640

--- QZBQuizBattle/QZBUserNameTextField.m
+++ QZBQuizBattle/QZBUserNameTextField.m
@@ -14,7 +14,7 @@
     NSString *candidate = self.text;
     
     NSString *nameRegex =
-    @"[A-Z0-9a-z._%+-]{2,20}";  //([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})
+    @"[A-Z0-9a-z_]{2,20}";  //([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})
     NSPredicate *nameTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", nameRegex];
     return [nameTest evaluateWithObject:candidate];
 
