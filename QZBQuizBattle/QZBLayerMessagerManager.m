bfaf881cf43f48674ca03129f9e7fcf46e535170
andreymikhaylov <drumih@gmail.com>
2015-08-26 15:24:35 +0300

    bug fixes
    Former-commit-id: 6132490d5d84723c4668a7ab3516bee23daeab0a
    Former-commit-id: de11fb8c85e09b88fcc0b16da08ccdf6bbfbbde4
    Former-commit-id: 7c5c407ebb1946288b1df0a8e470597ef51f62b8 [formerly 0e1ca08f3f2ea6c39731cb1ae3cfd491b53731a1]
    Former-commit-id: a1d92d74c0c4f61a868a98880c3e5a0ca449e244

--- QZBQuizBattle/QZBLayerMessagerManager.m
+++ QZBQuizBattle/QZBLayerMessagerManager.m
@@ -16,13 +16,13 @@
  Layer App ID from developer.layer.com
  */
 
-//#if QZB_PRODUCTION
+#if QZB_PRODUCTION
 static NSString *const LQSLayerAppIDString =
     @"layer:///apps/production/7523431a-3ba1-11e5-85e6-2d4d7f0072d6";
-//#else
-// static NSString *const LQSLayerAppIDString =
-// @"layer:///apps/staging/75233f64-3ba1-11e5-81a5-2d4d7f0072d6";
-//#endif
+#else
+ static NSString *const LQSLayerAppIDString =
+ @"layer:///apps/staging/75233f64-3ba1-11e5-81a5-2d4d7f0072d6";
+#endif
 //
 //#if TARGET_IPHONE_SIMULATOR
 //// If on simulator set the user ID to Simulator and participant to Device
@@ -58,6 +58,7 @@ static NSString *const LQSLayerAppIDString =
 - (instancetype)init {
     self = [super init];
     if (self) {
+        
         NSURL *appID = [NSURL URLWithString:LQSLayerAppIDString];
         self.layerClient = [LYRClient clientWithAppID:appID];
         self.layerClient.delegate = self;
