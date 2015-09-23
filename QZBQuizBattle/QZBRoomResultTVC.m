899c83652710d3a09c5bbf3982c50fcaedc2e3c6
andreymikhaylov <drumih@gmail.com>
2015-09-23 19:54:11 +0300

    bottom view added

--- QZBQuizBattle/QZBRoomResultTVC.m
+++ QZBQuizBattle/QZBRoomResultTVC.m
@@ -36,6 +36,8 @@
 
 #import <UAAppReviewManager.h>
 
+#import "UIFont+QZBCustomFont.h"
+
 
 
 //cell identifiers
@@ -52,6 +54,8 @@ NSString *const QZBShowQuestionsFromRoomIdentifier = @"showQuestionsFromRoomIden
 @property (strong, nonatomic) NSNumber *roomSessionID;
 @property (strong, nonatomic) NSArray *questions;
 @property (strong, nonatomic) QZBGameTopic *topic;
+
+@property (strong, nonatomic) UIView *bottomView;
 
 @end
 
@@ -72,7 +76,7 @@ NSString *const QZBShowQuestionsFromRoomIdentifier = @"showQuestionsFromRoomIden
                             action:@selector(reloadRoom)
                   forControlEvents:UIControlEventValueChanged];
     self.refreshControl.tintColor = [UIColor whiteColor];
-    
+    self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 40, 0);
     
     self.tableView.tableFooterView = [[UIView alloc] init];
     
@@ -88,7 +92,7 @@ NSString *const QZBShowQuestionsFromRoomIdentifier = @"showQuestionsFromRoomIden
     self.questions = [[QZBSessionManager sessionManager] sessionQuestions];
     self.topic = [QZBSessionManager sessionManager].topic;
     [[QZBSessionManager sessionManager] closeSession];
-    [self addBarButtonRight];
+  //  [self addBarButtonRight];
     
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(oneOfUsersFinishedRoom:)
@@ -98,6 +102,8 @@ NSString *const QZBShowQuestionsFromRoomIdentifier = @"showQuestionsFromRoomIden
 
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
+    
+//    [self.tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:0] atScrollPosition:UITableViewScrollPositionTop animated:YES];
     
 //    self.roomWorker = [QZBSessionManager sessionManager].roomWorker;
 //    
@@ -126,6 +132,7 @@ NSString *const QZBShowQuestionsFromRoomIdentifier = @"showQuestionsFromRoomIden
 -(void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
+    [self animateUp];
   //  [UAAppReviewManager showPromptIfNecessary];
     
 }
@@ -374,5 +381,90 @@ NSString *const QZBShowQuestionsFromRoomIdentifier = @"showQuestionsFromRoomIden
 -(void)showReportScreen {
     [self performSegueWithIdentifier:QZBShowQuestionsFromRoomIdentifier sender:nil];
 }
+
+#pragma mark - bottom view
+
+-(UIView *)bottomView{
+    if(!_bottomView){
+        
+        CGRect r = [UIScreen mainScreen].bounds;
+        
+        //CGSize navRect = self.navigationController.view.frame.size;
+        CGRect destRect = CGRectMake(0, r.size.height, r.size.width, 60);
+        
+        UIView *v = [[UIView alloc] initWithFrame:destRect];
+        //   UIColor *firstColor = [UIColor colorWithRed:31.0/255.0 green:181.0/255.0 blue:215.0/255.0 alpha:1];
+        //        UIColor *secondColor = [UIColor colorWithRed:254.0/255.0
+        //                                               green:204/255.0
+        //                                                blue:81.0/255.0
+        //                                               alpha:1.0];
+        UIColor *thirdColor = [UIColor colorWithRed:22.0/255.0
+                                              green:131.0/255.0
+                                               blue:199.0/255.0
+                                              alpha:1];
+        v.backgroundColor = thirdColor;//[UIColor colorWithRed:31.0/255.0 green:181.0/255.0 blue:215.0/255.0 alpha:1];
+        v.layer.cornerRadius = 5.0;
+        v.layer.masksToBounds = YES;
+        _bottomView = v;
+        //        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10, 0, r.size.width-10, 80)];
+        //        label.font = [UIFont systemFontOfSize:20];
+        //        label.textColor = [UIColor whiteColor];
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        
+        button.frame = CGRectMake(0, 0, r.size.width -  10, 40);
+        [button setTitle:@"Пожаловаться на вопрос" forState:UIControlStateNormal];
+        button.titleLabel.adjustsFontSizeToFitWidth = YES;
+        button.titleLabel.minimumScaleFactor = 0.5;
+        [button setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        button.titleLabel.font = [UIFont boldMuseoFontOfSize:22];//boldSystemFontOfSize:20];
+        
+        
+        [button setBackgroundColor:thirdColor];
+        button.layer.cornerRadius = 2.0;
+        button.layer.shadowOffset = CGSizeMake(0, 2);
+        button.layer.shadowColor = [UIColor blackColor].CGColor;
+        button.layer.shadowOpacity = 0.01;
+        
+        
+        [button addTarget:self action:@selector(showReportScreen)
+         forControlEvents:UIControlEventTouchUpInside];
+        
+        
+        [v addSubview:button];
+        
+    }
+    
+    return _bottomView;
+    
+}
+
+-(void)animateUp {
+    CGRect r = self.view.frame;//[UIScreen mainScreen].bounds;
+    [self.view addSubview:self.bottomView];
+    [self.view bringSubviewToFront:self.bottomView];
+    [UIView animateWithDuration:0.3
+                     animations:^{
+                         self.bottomView.frame = CGRectMake(0,
+                                                            self.tableView.contentOffset.y +
+                                                            self.tableView.frame.size.height -
+                                                            self.bottomView.frame.size.height+20,
+                                                            r.size.width,
+                                                            60);
+                     }];
+}
+
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if(_bottomView){
+        CGRect frame = self.bottomView.frame;
+        frame.origin.y = scrollView.contentOffset.y + self.tableView.frame.size.height - self.bottomView.frame.size.height+20;
+        self.bottomView.frame = frame;
+        
+        [self.view bringSubviewToFront:self.bottomView];
+    }
+    
+}
+
+
 
 @end
