44b05143c15bbb4cae12509e4dd432f283464681
andreymikhaylov <drumih@gmail.com>
2015-09-29 19:34:16 +0300

    mvp room messager

---
+++ QZBQuizBattle/QZBRoomFakeKeyboard.h
@@ -0,0 +1,14 @@
+//
+//  QZBRoomFakeKeyboard.h
+//  QZBQuizBattle
+//
+//  Created by Andrey Mikhaylov on 29/09/15.
+//  Copyright © 2015 Andrey Mikhaylov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QZBRoomFakeKeyboard : UIView
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *phrasesButtons;
+
+@end
