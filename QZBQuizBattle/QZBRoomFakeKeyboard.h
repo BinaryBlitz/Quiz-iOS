257b4953b057dd8d03c01c9458fe01e59f18be8c
andreymikhaylov <drumih@gmail.com>
2015-09-30 13:27:40 +0300

    room messenger improved
    Former-commit-id: 837f3aa86ff7b5f8df2c2c6601752b7f3a00bf9c
    Former-commit-id: 09704512d35ca5093196a7851deb721de5e51596
    Former-commit-id: e163c36f756c688abc098a0dde9ba1d9d924c862 [formerly 12ab230c2641be8a0f01ceb412185dc73846305f]
    Former-commit-id: 00a22d20aac6bbdf4ba95184edf83699edd60918

--- QZBQuizBattle/QZBRoomFakeKeyboard.h
+++ QZBQuizBattle/QZBRoomFakeKeyboard.h
@@ -10,5 +10,6 @@
 
 @interface QZBRoomFakeKeyboard : UIView
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *phrasesButtons;
+@property (weak, nonatomic) IBOutlet UIButton *closeButton;
 
 @end
