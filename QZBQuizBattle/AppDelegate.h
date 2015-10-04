7e0a57f892efb747afc9abc4ac21f4eec9328372
andreymikhaylov <drumih@gmail.com>
2015-10-04 19:23:32 +0300

    before merge
    Former-commit-id: 18c5cf64dac919c82f5b54c4a2050bb3fba23fd0
    Former-commit-id: f127f8ad7c5110bb416cf747d51afece99f44622
    Former-commit-id: b9935b6e9a5aefea9792e7a8e1508318b5389125 [formerly 1db31e2269e50d8798b5daeebd68e2c7469c0178]
    Former-commit-id: f500c2434959adbbc40593da4000f6178831ca93

--- QZBQuizBattle/AppDelegate.h
+++ QZBQuizBattle/AppDelegate.h
@@ -7,6 +7,7 @@
 //  Created by Andrey Mikhaylov on 11/12/14.
 //  Copyright (c) 2014 Andrey Mikhaylov. All rights reserved.
 //
+// before merge
 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
