337c63bfe15ea5aae42fd5ed8783b0558612121f
andreymikhaylov <drumih@gmail.com>
2015-10-04 15:36:18 +0300

    pod fabric
    Former-commit-id: bfc975abd13ccf8ac0a7d61992380e92026e2a9d
    Former-commit-id: 2a380ee38698ab21ad34544e15c675a4a8722fb3
    Former-commit-id: b910cd358aefa25266db3918bc8520b2a0d79ddc [formerly 3d1843ca6703c5fd65ae49b750f657de74570b42]
    Former-commit-id: 3dfdf236578161ff63ae2b448bed002fd854496f

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -66,6 +66,12 @@
 #define COCOAPODS_VERSION_MINOR_CocoaLumberjack_Extensions 9
 #define COCOAPODS_VERSION_PATCH_CocoaLumberjack_Extensions 2
 
+// Crashlytics
+#define COCOAPODS_POD_AVAILABLE_Crashlytics
+#define COCOAPODS_VERSION_MAJOR_Crashlytics 3
+#define COCOAPODS_VERSION_MINOR_Crashlytics 3
+#define COCOAPODS_VERSION_PATCH_Crashlytics 4
+
 // DBCamera
 #define COCOAPODS_POD_AVAILABLE_DBCamera
 #define COCOAPODS_VERSION_MAJOR_DBCamera 2
@@ -131,6 +137,18 @@
 #define COCOAPODS_VERSION_MAJOR_FLAnimatedImage 1
 #define COCOAPODS_VERSION_MINOR_FLAnimatedImage 0
 #define COCOAPODS_VERSION_PATCH_FLAnimatedImage 8
+
+// Fabric
+#define COCOAPODS_POD_AVAILABLE_Fabric
+#define COCOAPODS_VERSION_MAJOR_Fabric 1
+#define COCOAPODS_VERSION_MINOR_Fabric 5
+#define COCOAPODS_VERSION_PATCH_Fabric 5
+
+// Fabric/Base
+#define COCOAPODS_POD_AVAILABLE_Fabric_Base
+#define COCOAPODS_VERSION_MAJOR_Fabric_Base 1
+#define COCOAPODS_VERSION_MINOR_Fabric_Base 5
+#define COCOAPODS_VERSION_PATCH_Fabric_Base 5
 
 // GPUImage
 #define COCOAPODS_POD_AVAILABLE_GPUImage
